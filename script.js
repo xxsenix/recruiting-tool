ffab77eaf62f97f9509f352a21df11900a7a1bbf
Travis Callahan <callahantravis29@gmail.com>
2018-12-06 23:06:57 -0600

    added function getRepo which fetches from GitHubs List user repositories API endpoint

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 
+
+function getRepo(repoName) {
+    const url = `https://api.github.com/users/${repoName}/repos`;
+
+    fetch(url)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => console.log(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
+}
+
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
         const repoName = $('#js-search-user').val();
-        console.log(repoName);
+        getRepo(repoName);
     })
 }
 
